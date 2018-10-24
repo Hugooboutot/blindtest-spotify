5770b2d23ce36738737a8dad920bf12463ffc3f8
Mamad <diallomamadoury98@gmail.com>
2018-10-24 20:34:50 +0200

    Updates

---
+++ precache-manifest.e6cc3f22f8f92b1172cc8cd6446349db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bda8c1f1196ac6da745",
+    "url": "/blindtest-spotify/static/css/main.d1d361b7.chunk.css"
+  },
+  {
+    "revision": "1bda8c1f1196ac6da745",
+    "url": "/blindtest-spotify/static/js/main.1bda8c1f.chunk.js"
+  },
+  {
+    "revision": "3f7525847cbf7fd2fe0c",
+    "url": "/blindtest-spotify/static/js/1.3f752584.chunk.js"
+  },
+  {
+    "revision": "bd4de7ae32d74b318af5",
+    "url": "/blindtest-spotify/static/js/runtime~main.bd4de7ae.js"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "583522b60ac72b02c3905c08b5ad0fc3",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
